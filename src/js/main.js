c87198a494d9d10b4e4363ec5fcbb611403116b1
Maciek Boniaszczuk <maciek6001@gmail.com>
2019-07-30 11:38:15 +0200

    html and js structure

--- src/js/main.js
+++ src/js/main.js
@@ -3,20 +3,43 @@
 // service worker registration - remove if you're not going to use it
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('serviceworker.js').then(function (registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
+    }, function (err) {
       // registration failed :(
       console.log('ServiceWorker registration failed: ', err);
     });
   });
 }
 
-// place your code below
+// queryselectory
+const value = document.querySelector('.water__number--js')
+value.innerHTML = '0'
 
 
-console.log(`Hello world!`);
+const add__button = document.querySelector('.add__button--js');
+
+add__button.addEventListener('click', addCup)
+
+function addCup() {
+  const number = document.querySelector('.water__number--js')
+  number.innerHTML = parseInt(value.innerHTML) + 1;
+}
+
+const remove__button = document.querySelector('.remove__button--js');
+
+remove__button.addEventListener('click', removeCup)
 
 
+function removeCup() {
+  const currentValue = parseInt(value.innerHTML);
+  if (currentValue > 0) {
+    const number = document.querySelector('.water__number--js')
+    number.innerHTML = parseInt(value.innerHTML) - 1;
+  } else {
+    const number = document.querySelector('.water__number--js')
+    number.innerHTML = '0';
+  }
+}
